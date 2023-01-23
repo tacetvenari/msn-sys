659bf49b41c1e5678f200a65cd62baa41382ab59
Jesse Florig <jesseflorig@gmail.com>
2023-01-23 20:33:56 +0100

    merged servers

--- msn-server/msn-server.py
+++ msn-server/msn-server.py
@@ -1,65 +1,64 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from sys import argv
 from urllib.request import urlopen
+import asyncio
+import websockets
 import json
 
+MSN_DATA_FILE = 'msn-data.json'
 DATA_SERVERS = [
-        "http://localhost:8008",
-        "http://localhost:8009",
-        "http://localhost:8010",
-        "http://localhost:8011"
-    ]
+    "http://localhost:8008",
+    "http://localhost:8009",
+    "http://localhost:8010",
+    "http://localhost:8011"
+]
 
-class Server(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
+connected = set()
 
-    def do_HEAD(self):
-        self._set_headers()
+async def socket_handler(websocket):
+    # Register connection
+    connected.add(websocket)
 
-    # GET sends back a Hello world message
-    def do_GET(self):
-        with open('msn-data.json') as file:
-            data = json.load(file)
-            self._set_headers()
-            self.wfile.write(json.dumps(data).encode('utf-8'))
-            file.close()
+    try:
+        async for message in websocket:
+            print(f"Received message: {message}")
+            # Determine action based on message
+            if message == "get":
+                with open(MSN_DATA_FILE) as file:
+                    data = json.load(file)
+                    websockets.broadcast(connected, str(data))
+                    file.close()
+            elif message == "fetch":
+                with open(MSN_DATA_FILE, 'w') as msn_data_file:
+                    msn_data_file.write(f"[\n")
+                    for idx, data_server in enumerate(DATA_SERVERS):
+                        content = urlopen(data_server).read().decode()
+                        if (idx + 1) == len(DATA_SERVERS):
+                            msn_data_file.write(f"{content}\n")
+                        else:
+                            msn_data_file.write(f"{content},\n")
+                    msn_data_file.write(f"]\n")
+                    msn_data_file.close()
+                websockets.broadcast(connected, "{'fetched': true}")
+            elif message == "reset":
+                with open(MSN_DATA_FILE, 'w') as msn_data_file:
+                    msn_data_file.write(f"[]")
+                    msn_data_file.close()
+                websockets.broadcast(connected, "{'reset': true}")
+            #else:
+            #    websockets.broadcast(connected, f"Command '{message}' not recognized...")
 
-    def do_POST(self):
-        post_data = self.rfile.read(int(self.headers.get('Content-Length'))).decode()
-        if post_data == "fetch":
-            with open('msn-data.json', 'w') as msn_data_file:
-                msn_data_file.write(f"[\n")
-                for idx, data_server in enumerate(DATA_SERVERS):
-                    content = urlopen(data_server).read().decode()
-                    if (idx + 1) == len(DATA_SERVERS):
-                        msn_data_file.write(f"{content}\n")
-                    else:
-                        msn_data_file.write(f"{content},\n")
-                msn_data_file.write(f"]\n")
-                self._set_headers()
-                self.wfile.write("{\"mission_fetch\": \"complete\"}".encode('utf-8'))
-                msn_data_file.close()
+    finally:
+        # Unregister connection
+        connected.remove(websocket)
 
-        elif post_data == "reset":
-            with open('msn-data.json', 'w') as msn_data_file:
-                msn_data_file.write(f"[]")
-                self._set_headers()
-                self.wfile.write("{\"reset\": \"complete\"}".encode('utf-8'))
-                msn_data_file.close()
+async def main(port=8888):
+    print(f'Starting websocket server on port {port}...')
+    async with websockets.serve(socket_handler, "localhost", port):
+        await asyncio.Future()  # run forever
 
-def run(server_class=HTTPServer, handler_class=Server, port=8888):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-
-    print(f'Starting httpd on port {port}...')
-    httpd.serve_forever()
 
 if __name__ == "__main__":
-    from sys import argv
-
     if len(argv) == 2:
-        run(port=int(argv[1]))
+        asyncio.run(main(port=int(argv[1])))
     else:
-        run()
+        asyncio.run(main())
