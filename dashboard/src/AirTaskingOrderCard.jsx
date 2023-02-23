c968fe3607e52b201cc2a1a171d4f59eee2b6cc2
Jesse Florig <jesseflorig@gmail.com>
2023-02-23 13:56:40 +0100

    msn card reformat

--- dashboard/src/AirTaskingOrderCard.jsx
+++ dashboard/src/AirTaskingOrderCard.jsx
@@ -6,23 +6,37 @@ import {
   Heading,
   HStack,
   Stack,
-  Text
+  Text,
 } from '@chakra-ui/react'
 
 const {VITE_MSN_NUMBER, VITE_MSN_PLATFORM, VITE_MSN_TAKEOFF, VITE_MSN_RETURN, VITE_SHOPS} = import.meta.env
 const SHOPS = VITE_SHOPS.split(' ')
 
-function MissionMeta({target}){
+function MetaSection({label, value}){
   const metaFontSize = "1.5em"
+  return (
+    <HStack>
+      <Text fontSize={metaFontSize} fontWeight="bold">{`${label}:`}</Text>
+      <Text fontSize={metaFontSize}>{value.toUpperCase()}</Text>
+    </HStack>
+  )
+}
+
+MetaSection.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired
+}
+
+function MissionMeta({target}){
   return (
     <Stack>
       <Heading size="md">Mission Data</Heading>
       <Divider />
-      <HStack alignItems="flex-start" spacing={16}>
-        <Text fontSize={metaFontSize}>{`Takeoff: ${VITE_MSN_TAKEOFF}`}</Text>
-        <Text fontSize={metaFontSize}>{`Return: ${VITE_MSN_RETURN}`}</Text>
-        <Text fontSize={metaFontSize}>{`Platform: ${VITE_MSN_PLATFORM.toUpperCase()}`}</Text>
-        <Text fontSize={metaFontSize}>{`Target: ${target.toUpperCase()}`}</Text>
+      <HStack justifyContent="center" spacing={16}>
+        <MetaSection label="Takeoff" value={VITE_MSN_TAKEOFF} />
+        <MetaSection label="Return" value={VITE_MSN_RETURN} />
+        <MetaSection label="Platform" value={VITE_MSN_PLATFORM} />
+        <MetaSection label="Target" value={target} />
       </HStack>
     </Stack>
   )
@@ -37,6 +51,9 @@ MissionMeta.propTypes = {
 }
 
 function Status({label, state, delay}){
+  // TODO: Set timeout to remove animation so it reruns on rerender
+  // Random key is not viable - prevents transition from occurring
+
   const statusColor = {
     "good": "green.500",
     "unk": "gray.500"
@@ -44,7 +61,7 @@ function Status({label, state, delay}){
 
   return (
     <HStack>
-      <Circle size={10} bg={statusColor[state]} transition="all 0.5s" transitionDelay={`${delay}s`} />
+      <Circle size={10} bg={statusColor[state]} transition={`all 0.5s ${delay}s`} />
       <Text fontSize="2em" fontWeight="bold">{label.toUpperCase()}</Text>
     </HStack>
   )
@@ -61,7 +78,7 @@ function MissionStatus({states}){
     <Stack>
       <Heading size="md">Mission Status</Heading>
       <Divider />
-      <HStack spacing={4}>
+      <HStack justifyContent="center" spacing={8}>
         {SHOPS.map((shop, idx) => {
           const currentState = states[shop] ? states[shop].status : 'unk'
           const delay = idx * 0.5
